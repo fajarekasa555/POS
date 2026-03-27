3a60e4f0264265dc6cc0579795ea3e9f212044c3
fajareka1 <fajareka.haltec@gmail.com>
2026-03-28 06:01:32 +0700

    commit : p4 separuh

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,13 +9,25 @@ class UserController extends Controller
 {
     public function index()
     {
-        $data = [
-            'nama' => 'Fajar Eka',
-        ];
+        // $data = [
+        //     'nama' => 'Fajar Eka',
+        // ];
 
-        UserModel::where('username', 'customer-1')->update($data);
+        // UserModel::where('username', 'customer-1')->update($data);
 
-        $user = UserModel::all();
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'password' => Hash::make('12345'),
+        //     'nama' => 'Manager 3',
+        // ];
+
+        // UserModel::create($data);
+
+        // $user = UserModel::where('level_id', 1)->first();
+
+        // $user = UserModel::where('username', 'manager9')->firstOrFail();
+        $user = UserModel::where('level_id', 2)->count();
         return view('user', ['data' => $user]);
     }
 }
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -10,19 +10,31 @@
     <h1>Data User</h1>
     <table border="1" cellpadding="10" cellspacing="0">
         <tr>
+            <th>Jumlah Pengguna</th>
+        </tr>
+        <tr>
+            <td>{{ $data }}</td>
+        </tr>
+        {{-- <tr>
             <th>ID</th>
             <th>Username</th>
             <th>Nama</th>
             <th>Level Pengguna</th>
         </tr>
-        @foreach ($data as $level)
+        <tr>
+            <td>{{ $data->user_id }}</td>
+            <td>{{ $data->username }}</td>
+            <td>{{ $data->nama }}</td>
+            <td>{{ $data->level_id }}</td>
+        </tr> --}}
+        {{-- @foreach ($data as $level)
         <tr>
             <td>{{ $level->user_id }}</td>
             <td>{{ $level->username }}</td>
             <td>{{ $level->nama }}</td>
             <td>{{ $level->level_id }}</td>
         </tr>
-        @endforeach
+        @endforeach --}}
     </table>
 </body>
 </html>
